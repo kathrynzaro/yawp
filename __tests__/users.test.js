cedf48944bcd3a095031631b3d60318a83e8102f
kathrynzaro <zarokathryn@gmail.com>
2022-08-15 14:45:52 -0700

    create user test passing

---
+++ __tests__/users.test.js
@@ -0,0 +1,37 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
+
+describe('yawp user routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  
+  afterAll(() => {
+    pool.end();
+  });
+  
+  it('#POST should create a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({
+      message: 'You\'ve successfully signed in',
+      user: {
+        id: expect.any(String),
+        firstName,
+        lastName,
+        email,
+      },
+    });
+  });
+  
+});
